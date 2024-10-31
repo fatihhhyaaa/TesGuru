d4438246a52614c2b9b5cecda9bdd54b3b430aa1
fatihhhyaaa <friskiramdhani@gmail.com>
2024-10-31 11:51:03 +0700

    Add files via upload

---
+++ Guru.java
@@ -0,0 +1,25 @@
+public class Guru{
+    int id;
+    String nama, mapel, alamat;
+
+
+    /*Constuctor nama sama dgn nama Class */
+    public Guru () {
+    id  = 0;
+     nama = "";
+    mapel = "";
+    alamat = "";
+
+    }
+
+}
+   
+
+
+
+
+    
+
+    
+    
+
